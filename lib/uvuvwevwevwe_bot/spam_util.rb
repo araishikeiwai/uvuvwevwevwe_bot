c49696f6f38742f2f01f646ada37a6f445a4614a
Rick Daniel <rick@araishikeiwai.com>
2016-12-21 22:38:10 +0700

    subhanallah

--- lib/uvuvwevwevwe_bot/spam_util.rb
+++ lib/uvuvwevwevwe_bot/spam_util.rb
@@ -4,7 +4,7 @@ class Object.const_get($namespace_class)::SpamUtil
     key = "#{$namespace}_spam_#{id}"
     $redis.incr(key)
     ttl = group ? 10 : 30
-    $redis.expire(key) if $redis.ttl(key) < 0
+    $redis.expire(key, ttl) if $redis.ttl(key) < 0
     $redis.get(key).to_i <= 10
   end
 end
